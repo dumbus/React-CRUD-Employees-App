ff76cd1d11fafcdebb21af3eb523b8820dc2effd
dumbus <dumbusx@gmail.com>
2022-03-14 18:24:18 +0300

    feat: add events for form component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,11 +9,11 @@ import './app.css';
 function App() {
 
   const data = [
-    {name: 'John C.', salary: 3000, increase: false, id: 1},
-    {name: 'Alex S.', salary: 800, increase: false, id: 2},
-    {name: 'Adam W.', salary: 1000, increase: false, id: 3},
-    {name: 'Max D.', salary: 1200, increase: false, id: 4},
-    {name: 'Michel S.', salary: 1500, increase: true, id: 5}
+    {name: 'John C.', salary: 3000, id: 1},
+    {name: 'Alex S.', salary: 800, id: 2},
+    {name: 'Adam W.', salary: 1000, id: 3},
+    {name: 'Max D.', salary: 1200, id: 4},
+    {name: 'Michel S.', salary: 1500, id: 5}
   ];
 
   return(
