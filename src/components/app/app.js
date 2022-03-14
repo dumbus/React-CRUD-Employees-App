f30741caae06c2f9a849ef23eaf32eab859d09d6
dumbus <dumbusx@gmail.com>
2022-03-14 18:52:26 +0300

    feat: add deletion of employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,29 +8,45 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John C.', salary: 3000, id: 1},
+        {name: 'Alex S.', salary: 800, id: 2},
+        {name: 'Adam W.', salary: 1000, id: 3},
+        {name: 'Max D.', salary: 1200, id: 4},
+        {name: 'Michel S.', salary: 1500, id: 5}
+      ]
+    }
+  }
 
-  const data = [
-    {name: 'John C.', salary: 3000, id: 1},
-    {name: 'Alex S.', salary: 800, id: 2},
-    {name: 'Adam W.', salary: 1000, id: 3},
-    {name: 'Max D.', salary: 1200, id: 4},
-    {name: 'Michel S.', salary: 1500, id: 5}
-  ];
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
-  return(
-    <div className="app">
-      <AppInfo />
-
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  render() {
+    return(
+      <div className="app">
+        <AppInfo />
+  
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+  
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem}/>
+        <EmployeesAddForm />
       </div>
-
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,13 +2,16 @@ import EmployeesListItem from "../employees-list-item/employees-list-item"
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
 
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
   
     return (
-      <EmployeesListItem key={id} {...itemProps}/>
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}/>
     );
   });
 
