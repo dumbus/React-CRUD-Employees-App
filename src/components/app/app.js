78d300057d44aa23e73e659e5300716ddd864daf
dumbus <dumbusx@gmail.com>
2022-03-14 22:19:14 +0300

    feat: add total and rised employees number

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,11 +13,11 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name: 'John C.', salary: 3000, id: 1},
-        {name: 'Alex S.', salary: 800, id: 2},
-        {name: 'Adam W.', salary: 1000, id: 3},
-        {name: 'Max D.', salary: 1200, id: 4},
-        {name: 'Michel S.', salary: 1500, id: 5}
+        {name: 'John C.', salary: 3000, increase: false, rise: true, id: 1},
+        {name: 'Alex S.', salary: 800, increase: true, rise: false, id: 2},
+        {name: 'Adam W.', salary: 1000, increase: false, rise: false, id: 3},
+        {name: 'Max D.', salary: 1200, increase: false, rise: false, id: 4},
+        {name: 'Michel S.', salary: 1500, increase: false, rise: false, id: 5}
       ]
     }
     this.maxId = 6;
@@ -28,6 +28,7 @@ class App extends Component {
       name: name,
       salary: salary,
       increase: false,
+      rise: false,
       id: this.maxId++
     }
 
@@ -48,10 +49,26 @@ class App extends Component {
     })
   }
 
+  onToggleProp = (id, prop) => {
+    this.setState(({data}) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return {...item, [prop]: !item[prop]}
+        }
+        return item;
+      })
+    }))
+  }
+
   render() {
+    const total = this.state.data.length;
+    const increased = this.state.data.filter((item) => item.increase).length;
+
     return(
       <div className="app">
-        <AppInfo />
+        <AppInfo
+          total={total}
+          increased={increased}/>
   
         <div className="search-panel">
           <SearchPanel />
@@ -60,7 +77,8 @@ class App extends Component {
   
         <EmployeesList
           data={this.state.data}
-          onDelete={this.deleteItem}/>
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}/>
         <EmployeesAddForm onAdd={this.addItem}/>
       </div>
     );
